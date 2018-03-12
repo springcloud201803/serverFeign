45afe0bcd8a8023963aada6f455c7eb9dcbfab15
painAdmin <1767123925@qq.com>
2018-03-12 14:39:16 +0800

    增加类注释

--- src/main/java/com/example/demo/ServerFeignApplication.java
+++ src/main/java/com/example/demo/ServerFeignApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-
+/**
+ * feign  远程调用
+ * @author pain
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
